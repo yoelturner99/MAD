5fd3e68c4648bb79b33d7eec2648cbdd3424c068
yoelturner99 <yoelturner99@gmail.com>
2024-01-22 12:37:50 +0100

    Add settings in __init__.py

--- scripts/run_bot.py
+++ scripts/run_bot.py
@@ -1,26 +1,14 @@
 # -*- coding: utf-8 -*-
-import os
+import sys
 import logging
+sys.path.insert(0, './src')
 
 import discord
-from pydub import AudioSegment
-from dotenv import load_dotenv
 
 from MAD.bot import MAD_Bot
 from MAD.model import MAD_Classifier
 from MAD.database import MAD_Database
-
-# Set environment variables
-load_dotenv()
-FS_ROOT = os.path.abspath(os.sep)
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-DATABASE_FILE = os.getenv("DATABASE_FILE")
-MODEL_DIR = os.getenv("MODEL_DIR")
-
-# Set ffmpeg bin
-AudioSegment.converter = os.path.join(FS_ROOT, "ffmpeg", "bin", "ffmpeg.exe")
-AudioSegment.ffmpeg = os.path.join(FS_ROOT, "ffmpeg", "bin", "ffmpeg.exe")
-AudioSegment.ffprobe = os.path.join(FS_ROOT, "ffmpeg", "bin", "ffprobe.exe")
+from MAD import DISCORD_TOKEN, DATABASE_FILE, MODEL_DIR
 
 if __name__ == "__main__":
     handler = logging.FileHandler(
