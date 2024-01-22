afd9de1d8895ab7a57a9ee0ec74c9f19df2a6f8d
yoelturner99 <yoelturner99@gmail.com>
2024-01-22 12:45:35 +0100

    Update run_bot.py

--- scripts/run_bot.py
+++ scripts/run_bot.py
@@ -8,22 +8,31 @@ import discord
 from MAD.bot import MAD_Bot
 from MAD.model import MAD_Classifier
 from MAD.database import MAD_Database
-from MAD import DISCORD_TOKEN, DATABASE_FILE, MODEL_DIR
+from MAD import (
+    MODEL_DIR,
+    DISCORD_TOKEN,
+    DATABASE_FILE,
+)
 
 if __name__ == "__main__":
+    # Initialize classifier and database
+    classifier = MAD_Classifier(MODEL_DIR)
+    db = MAD_Database(DATABASE_FILE)
+    db.initialize_database()
+
+    # Initialize Client
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = MAD_Bot(intents, db, classifier)
+
+    # Add handler for logging
     handler = logging.FileHandler(
         filename='logs/discord.log',
         encoding='utf-8',
         mode='w'
     )
-    
-    classifier = MAD_Classifier(MODEL_DIR)
-    db = MAD_Database(DATABASE_FILE)
-    db.initialize_database()
 
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = MAD_Bot(intents, db, classifier)
+    # Run client 
     client.run(
         token=DISCORD_TOKEN,
         log_handler=handler,
